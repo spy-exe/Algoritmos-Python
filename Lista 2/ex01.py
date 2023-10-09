8b4806f73660974a65d3be5be177039757a7d32e
Ricardo R. Figueiredo <ra202310773@univassouras.edu.br>
2023-10-09 20:00:03 -0300

    Update - Lista 2

---
+++ Lista 2/ex01.py
@@ -0,0 +1,73 @@
+# Uma empresa de pesquisas precisa tabular os resultados da seguinte enquete feita a um grande quantidade de
+# organizações:
+# "Qual o melhor Sistema Operacional para uso em servidores?"
+# As possíveis respostas são:
+# 1- Windows Server
+# 2- Unix
+# 3- Linux
+# 4- Netware
+# 5- Mac OS
+# 6- Outro
+# Você foi contratado para desenvolver um programa que leia o resultado da enquete e informe ao final o resultado da
+# mesma. O programa deverá ler os valores até ser informado o valor 0, que encerra a entrada dos dados. Não
+# deverão ser aceitos valores além dos válidos para o programa (0 a 6). Os valores referentes a cada uma das opções
+# devem ser armazenados num vetor. Após os dados terem sido completamente informados, o programa deverá
+# calcular a percentual de cada um dos concorrentes e informar o vencedor da enquete. O formato da saída foi dado
+# pela empresa, e é o seguinte:
+# Sistema Operacional Votos %
+# ------------------- ----- ---
+# Windows Server 1500 17%
+# Unix 3500 40%
+# Linux 3000 34%
+# Netware 500 5%
+# Mac OS 150 2%
+# Outro 150 2%
+# ------------------- -----
+# Total 8800
+# O Sistema Operacional mais votado foi o Unix, com 3500 votos, correspondendo
+# a 40% dos votos.
+
+votos = { # Criando um dicionario com os sistemas operacionais - Melhor manipulacao de dados durante o codigo.
+    'Windows Server': 0,
+    'Unix': 0,
+    'Linux': 0,
+    'Netware': 0,
+    'Mac OS': 0,
+    'Outro': 0
+}
+
+while True:
+    voto = int(input('Qual o melhor S.O. para uso em servidores?'))
+    if voto == 0: # Se o voto for = 0, encerrar programa
+        break
+    elif voto > 6 or voto < 0: # Se o voto for maior que 6 ou menor que 0 = Voto Invalido
+        print('Voto inválido, tente novamente.')
+        continue
+    else:
+        if voto == 1: # Se o voto for igual a 1 = Windows Server
+            votos['Windows Server'] += 1
+        elif voto == 2:
+            votos['Unix'] += 1 
+        elif voto == 3:
+            votos['Linux'] += 1
+        elif voto == 4:
+            votos['Netware'] += 1
+        elif voto == 5:
+            votos['MacOS'] += 1
+        elif voto == 6:
+            votos['Outro'] += 1
+
+total = sum(votos.values())     # Numero total de votos.
+
+# Resultado da enquete
+print("Sistema Operacional   Votos   %")
+print("-------------------   -----   ---")
+for sistema, voto in votos.items():
+    percentual = (voto / total) * 100
+    print(f"{sistema:20} {voto:6} {percentual:.1f}%")
+print("-------------------   -----")
+print(f"Total                {total}")
+
+# S.O. vencedor da enquete
+vencedor = max(votos, key=votos.get)
+print(f"O S.O. mais votado foi o {vencedor}, com {votos[vencedor]} votos, correspondendo a {percentual:.1f}% dos votos.")
---
+++ Lista 2/ex03.py
@@ -0,0 +1,55 @@
+# Faça um programa que carregue uma lista com os modelos de cinco carros (exemplo de modelos: FUSCA, GOL,
+# VECTRA etc). Carregue uma outra lista com o consumo desses carros, isto é, quantos quilômetros cada um desses
+# carros faz com um litro de combustível. Calcule e mostre:
+# . O modelo do carro mais econômico;
+# a. Quantos litros de combustível cada um dos carros cadastrados consome para percorrer uma distância de
+# 1000 quilômetros e quanto isto custará, considerando um que a gasolina custe R$ 2,25 o litro. Abaixo
+# segue uma tela de exemplo. O disposição das informações deve ser o mais próxima possível ao exemplo.
+# Os dados são fictícios e podem mudar a cada execução do programa.
+# Comparativo de Consumo de Combustível
+# Veículo 1
+# Nome: fusca
+# Km por litro: 7
+# Veículo 2
+# Nome: gol
+# Km por litro: 10
+# Veículo 3
+# Nome: uno
+# Km por litro: 12.5
+# Veículo 4
+# Nome: Vectra
+# Km por litro: 9
+# Veículo 5
+# Nome: Peugeout
+# Km por litro: 14.5
+# Relatório Final
+# 1 - fusca - 7.0 - 142.9 litros - R$ 321.43
+# 2 - gol - 10.0 - 100.0 litros - R$ 225.00
+# 3 - uno - 12.5 - 80.0 litros - R$ 180.00
+# 4 - vectra - 9.0 - 111.1 litros - R$ 250.00
+# 5 - peugeout - 14.5 - 69.0 litros - R$ 155.17
+# O menor consumo é do peugeout.
+
+# Define listas para armazenar os modelos de cinco carros e seus respectivos consumos
+modelos = []
+consumo = []
+
+# Pede entrada para cada modelo de carro e seu consumo
+for i in range(5):
+    modelo = input(f"Veículo {i+1}\nNome: ")
+    km_por_litro = float(input("Km por litro: "))
+    modelos.append(modelo)
+    consumo.append(km_por_litro)
+
+# Calcula o consumo de combustível e o custo para cada carro percorrer 1000 km
+consumo_combustivel = [1000 / km for km in consumo]
+custo_combustivel = [consumo * 2.25 for consumo in consumo_combustivel]
+
+# Encontra o índice do carro com o menor consumo de combustível
+indice_menor_consumo = consumo_combustivel.index(min(consumo_combustivel))
+
+# Imprime o relatório final
+print("Comparativo de Consumo de Combustível")
+for i in range(5):
+    print(f"{i+1} - {modelos[i]} - {consumo[i]} - {consumo_combustivel[i]:.1f} litros - R$ {custo_combustivel[i]:.2f}")
+print(f"O menor consumo é do {modelos[indice_menor_consumo]}")
